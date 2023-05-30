e04936cf557980a144be169db7f8918915f275a2
Soham2623 <sokumar2023@gmail.com>
2023-05-30 16:49:38 +0200

    Seeing 0-99% range of values for weather availability

--- Quick sizing scripts/Weather.py
+++ Quick sizing scripts/Weather.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
-
 
 def weather_availability(percentage_duration):
     # Standard Deviations and averages
@@ -13,9 +13,9 @@ def weather_availability(percentage_duration):
     mu_wind = 7.891210879
 
     # alpha to critical
-    alpha = 1-(percentage_duration/100)
+    alpha = 1 - (percentage_duration / 100)
     n_sided = 1  # 2-sided test
-    z_crit = stats.norm.ppf(1-alpha/n_sided)
+    z_crit = stats.norm.ppf(1 - alpha / n_sided)
 
     # Result
     x_current = z_crit * sd_current + mu_current
@@ -24,14 +24,30 @@ def weather_availability(percentage_duration):
 
     return x_current, x_waves, x_wind
 
+rc = []
+rwave = []
+rwind = []
+num=[]
 
 if __name__ == '__main__':
 
-    result_current, result_waves, result_wind = weather_availability(float(input('Enter percentage of month (write digits e.g. 80): ',)))
+    # result_current, result_waves, result_wind = weather_availability(float(input('Enter percentage of month (write digits e.g. 80): ',)))
 
-    print('Must design for currents up to', result_current, 'm/s')
-    print('Must design for waves up to', result_waves, 'm')
-    print('Must design for wind up to', result_wind, 'm/s')
+    # print('Must design for currents up to', result_current, 'm/s')
+    # print('Must design for waves up to', result_waves, 'm')
+    # print('Must design for wind up to', result_wind, 'm/s')
 
+    for i in range(100):
+        result_current, result_waves, result_wind = weather_availability(i)
+        num.append(i)
+        rc.append(result_current)
+        rwave.append(result_waves)
+        rwind.append(result_wind)
 
-
+    plt.plot(num, rc, label='Currents')
+    plt.plot(num, rwave, label='Waves')
+    plt.plot(num, rwind, label='Wind')
+    plt.legend()
+    plt.xlabel("Percentage")
+    plt.ylabel("m/s")
+    plt.show()
